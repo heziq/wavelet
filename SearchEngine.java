badec87e82b1771a2ae3fecb90731312aa030017
Yaran Yang <yay025@UCSD.EDU>
2023-01-19 13:49:56 -0800

    Create SearchEngine.java

---
+++ SearchEngine.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import java.net.URI;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    String[] mystring = new Array[100];
+    int num = 0; 
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return String.format("Yaran Number: %d", num);
+        // } else if (url.getPath().equals("/increment")) {
+        //     num += 1;
+        //     return String.format("Number incremented!");
+        } else {
+            System.out.println("Path: " + url.getPath());
+            if (url.getPath().contains("/add")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    mystring[num] = parameters[1];
+                    num ++ ;
+                    return String.format("String is added by %s! ", parameters[1], num);
+                }
+            }
+            return "404 Not Found!";
+        }
+    }
+}
+
+class NumberServer {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
